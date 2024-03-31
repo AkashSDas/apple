a61254e7e6e42ba2509f973e06f9a586dad6cf92
AkashSDas <aakashdas368@gmail.com>
2024-03-31 13:08:21 +0530

    create Footer component and add it to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Highlight from "./components/Highlight";
 import HowItWorks from "./components/HowItWorks";
@@ -15,6 +16,7 @@ function App(): JSX.Element {
             <Model />
             <Features />
             <HowItWorks />
+            <Footer />
         </div>
     );
 }
